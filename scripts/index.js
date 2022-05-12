f3979ef6375a4dac4b1d5ff4b57d7794e9090935
MariKa <Marla202101@yandex.ru>
2022-05-12 17:34:45 +0200

    завершена вёрстка по макету

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let popup = document.querySelector('.popup');
+let openButton = document.querySelector('.profile__edit-button');
+let closeButton = popup.querySelector('.popup__close-icon');
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__name');
+let jobInput = formElement.querySelector('.popup__occupation');
+
+let nameText = document.querySelector('.profile__info-name');
+let jobText = document.querySelector('.profile__info-occupation');
+
+function togglePopup() {
+    nameInput.value = nameText.textContent;
+    jobInput.value = jobText.textContent;
+    popup.classList.toggle('popup_opened');
+};
+
+openButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    nameText.textContent = nameInput.value ;
+    jobText.textContent = jobInput.value;
+    togglePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
